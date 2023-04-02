7b397fa1d324e6dfde07954ab56899ba6c43fd3b
eueduardosou <89845144+eueduardosou@users.noreply.github.com>
2023-04-02 20:27:48 -0300

    adiciona página inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './components/tela-inicial/tela-inicial/tela-inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaInicialComponent
   ],
   imports: [
     BrowserModule,
